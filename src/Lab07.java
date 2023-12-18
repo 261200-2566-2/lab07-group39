5a2f9aedb83b1204921ac0f2a379855e48e6c647
Lolorpof <lolorpof@gmail.com>
2023-12-18 23:56:50 +0700

    fixed something

--- src/Lab07.java
+++ src/Lab07.java
@@ -6,14 +6,15 @@ public class Lab07 {
         HashSet<String> sth = new HashSet<>(10);
         hs.add("joe");
         hs.add("mama");
-        hs.add("joe");
         hs.add("lolz");
+
         sth.add("joe");
         sth.add("mama");
         sth.add("huh");
         sth.add("bruh");
         //Object[] arr = hs.toArray();
         System.out.println(hs);
+        System.out.println(sth);
         //hs.remove("lolz");
         //hs.addAll(sth);
         hs.retainAll(sth);
@@ -22,6 +23,7 @@ public class Lab07 {
         //hs.clear();
 
         System.out.println();
+        System.out.println(hs.size());
         System.out.println(hs.contains("lolz"));
         System.out.println(hs.containsAll(sth));
     }
